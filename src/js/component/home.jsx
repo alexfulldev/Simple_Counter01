e9a8c5fe1496db2c5d2da2aa2dfc76b2d885d126
alexfulldev <166410130+alexfulldev@users.noreply.github.com>
2024-07-11 23:50:29 +0000

    acabado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,6 +7,12 @@ import { counter } from "@fortawesome/fontawesome-svg-core";
 
 //create your first component
 const Home = () => {
+
+	const one = 1;
+	const two = 2;
+	const three = 3;
+	const four = 4;
+	
 	return(
 		<SimpleCounter digitOne={one} digittwo={two} digitthree={three} digitfour={four} />
 	);
--- src/js/component/simplecounter.jsx
+++ src/js/component/simplecounter.jsx
@@ -1,33 +1,36 @@
 import React from "react";
-import "../styles/index.css";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom/client"
 // Images
 
 
 // Component
 
 export function SimpleCounter(props) {
-    return(<div className="bigCounter">
+    return (<div className="bigCounter">
         <div className="calendar">
             <i className="fa-regular fa-clock"></i>
         </div>
-        <div className="seconds4">0{props.digitFour}</div>
+        <div className="seconds4">{props.digitFour}</div>
         <div className="seconds3">{props.digitThree}</div>
         <div className="seconds2">{props.digitTwo}</div>
         <div className="seconds1">{props.digitOne}</div>
     </div>);
 }
-SimpleCounter.propTypes={
-    digitFour:propTypes.number,
-    digitThree:propTypes.number,
-    digitTwo:propTypes.number,
-    digitOne:propTypes.number
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
 };
+const root = 
+ReactDOM.createRoot(document.getElementById('app'));
 let counter = 0;
-setInterval(function(){
-    const four= Math.Floor(counter/10000);
-    const three= Math.Floor(counter/1000);
-    const two= Math.Floor(counter/100);
-    const one= Math.Floor(counter/1);
-    print(four, three, two, one,);
+setInterval(function () {
+    const four = Math.floor((counter / 1000) % 10);
+    const three = Math.floor((counter / 100)% 10);
+    const two = Math.floor((counter / 10)% 10);
+    const one = Math.floor(counter % 10);
+    root.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three}digitFour={four}/>);
     counter++;
 },1000);
